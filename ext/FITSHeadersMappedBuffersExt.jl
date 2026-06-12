8b37b045e92e7c687a17ae1ba2351579649c7e78
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2026-06-12 18:56:29 +0200

    Move `MappedBuffers` extension into this package
    This solves for `MappedBuffers` not yet in the General registry.

--- ext/FITSHeadersMappedBuffersExt.jl
+++
@@ -1,9 +0,0 @@
-module FITSHeadersMappedBuffersExt
-
-using FITSHeaders
-using MappedBuffers
-
-FITSHeaders.Parser.PointerCapability(::Type{<:MappedBuffers.MappedBuffer}) =
-    FITSHeaders.Parser.PointerFull()
-
-end # module
